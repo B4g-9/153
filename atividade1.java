ca8866e1ded58eef11c053fb33b73dc98ece0e8e
B4g-9 <4542039519@estudante.sed.sc.gov.br>
2024-08-22 11:40:03 -0300

    Add files via upload

---
+++ atividade1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class atividade1 {
+
+  public static void main ( String[] args ){
+  System.out.println("Digite um numero de 1 a 5: ");
+  Scanner leia =new Scanner(System.in);
+   int numero = leia.nextInt();
+    
+   switch (numero) {
+
+
+    case 1:
+    System.out.println("Número 1 foi digitado...");
+    break;
+ 
+    case 2:
+    System.out.println("Número 2 foi digitado...");
+    break;
+ 
+    case 3:
+    System.out.println("Número 3 foi digitado...");
+    break;
+  
+    case 4:
+    System.out.println("Número 4 foi digitado...");
+    break;
+   
+    case 5:
+    System.out.println("Número 5 foi digitado...");
+    break;
+
+    default:
+    System.out.print("Número invalido... ");
+    break;
+    }
+   }
+    }
